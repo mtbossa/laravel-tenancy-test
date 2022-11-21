12a4ad36391dfdd1479360d9b4faecf4378b2af6
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-11-21 18:40:04 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->deleteTenantSchemas();
+
          \App\Models\User::factory(5)->create();
 
          \App\Models\User::factory()->create([
              'name' => 'Test User',
              'email' => 't@t',
          ]);
+
+        \App\Models\Tenant::create();
+        \App\Models\Tenant::create();
+
+        \App\Models\Tenant::all()->runForEach(function () {
+            \App\Models\User::factory(5)->create();
+        });
+    }
+
+    private function deleteTenantSchemas() {
+        $schemas = DB::select("SELECT schema_name FROM information_schema.schemata");
+
+        $tenantSchemas = Collection::make($schemas)
+            ->map(fn (\stdClass $schema) => $schema->schema_name)
+            ->filter(fn (string $schema_name) => Str::of($schema_name)->before('_') == 'tenant');
+
+        foreach ($tenantSchemas as $tenantSchema) {
+            DB::statement("drop schema \"$tenantSchema\" cascade");
+        }
+
     }
 }
